6e0a23a9be88cb58b2184bef5ef0d7e25acaa3d0
michal <hush.some1@gmail.com>
2019-04-18 17:02:28 +0000

    project

--- src/js/script.js
+++
@@ -1,5 +0,0 @@
-var x = "this is my first X"
-
-console.log(x + 'is my first x from first file')
-
-console.log("first file run")
--- src/js/script2.js
+++
@@ -1,5 +0,0 @@
-var x = 2
-
-console.log(x + 'is 2 from second file')
-
-console.log('this is second file')
